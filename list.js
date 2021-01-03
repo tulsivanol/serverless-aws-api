dbee17a54c8d75c080cc237d8c24b26f000297f4
tulsivanol <tulsivanol@gmail.com>
2021-01-03 11:08:32 +0530

    getting all notes

---
+++ list.js
@@ -0,0 +1,16 @@
+import handler from "./libs/handler-lib";
+import dynamoDB from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, ctx) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": "123",
+    },
+  };
+
+  const result = await dynamoDB.query(params);
+
+  return result.Items;
+});
